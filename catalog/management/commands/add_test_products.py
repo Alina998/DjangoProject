11d0596ddbfde3cdd149e5c1b472863eb1d6a878
our ame <pastaeva98@mail.ru>
2025-01-15 20:26:19 +0700

    homework_24

--- catalog/management/commands/add_test_products.py
+++ catalog/management/commands/add_test_products.py
@@ -14,7 +14,13 @@ class Command(BaseCommand):
         clothing = Category.objects.create(name='Одежда', description='Модная одежда')
 
         # Создание продуктов
-        Product.objects.create(name='Смартфон', description='Современный смартфон', category=electronics, price=699.99)
-        Product.objects.create(name='Футболка', description='Удобная футболка', category=clothing, price=19.99)
+        Product.objects.create(name='iPhone 13', description='Смартфон Apple iPhone 13 оснащен 6.1-дюймовым дисплеем OLED – энергоэффективным и контрастным даже при ярком солнечном свете.'
+                                                             'А прочный корпус надежно защищен от воды и пыли.', category=electronics, price=699.99)
+        Product.objects.create(name='Футболка', description='Предоставляем вашему вниманию стильную и удобную футболку оверсайз!'
+                                                            'Эта вещь станет незаменимым аксессуаром для тех, кто ценит комфорт и следит за трендами.', category=clothing, price=19.99)
+        Product.objects.create(name='Худи',
+                               description='Оверсайз худи, которое вы искали! Толстовка свободного силуэта, с увеличенной проймой, шириной рукавов и спущенной плечевой линией.'
+                                           'Базовая и универсальная толстовка с капюшоном выполнена из мягкого хлопка, приятна к телу и точно станет самой любимой.',
+                               category=clothing, price=19.99)
 
         self.stdout.write(self.style.SUCCESS('Тестовые продукты успешно добавлены.'))
--- catalog/views.py
+++ catalog/views.py
@@ -1,16 +1,43 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .models import Product
+from django import forms
 
+
+# def home(request):
+#     latest_products = Product.objects.order_by('-created_at')[:5]
+#     return render(request, 'home.html', {'latest_products': latest_products})
+
+
+from django.core.paginator import Paginator
 
 def home(request):
-    return render(request, 'home.html')
+    products = Product.objects.all()
+    return render(request, 'home.html', {'products': products})
 
-# def contacts(request):
-#     return render(request, 'contacts.html')
-#
-#
 
 def contacts(request):
     success = False
     if request.method == 'POST':
         success = True
     return render(request, 'contacts.html', {'success': success})
+
+
+def product_info(request):
+    products = Product.objects.all()
+    return render(request, 'product_info.html', {'products' : products})
+
+
+class ProductForm(forms.ModelForm):
+    class Meta:
+        model = Product
+        fields = ['name', 'description', 'image', 'category', 'price']
+
+def add_product(request):
+    if request.method == 'POST':
+        form = ProductForm(request.POST, request.FILES)
+        if form.is_valid():
+            form.save()
+            return redirect('home')  # Перенаправление на домашнюю страницу
+    else:
+        form = ProductForm()
+    return render(request, 'add_product.html', {'form': form})
