fe9a41efd7ded8cc0cd6ef1d9216d8702b51edca
our ame <pastaeva98@mail.ru>
2025-01-18 18:47:27 +0700

    add media, fixed code

--- catalog/urls.py
+++ catalog/urls.py
@@ -1,12 +1,15 @@
-from django.urls import path, include
+from django.urls import path
 from catalog.apps import CatalogConfig
 from catalog.views import home, contacts, product_info, add_product
+from django.conf import settings
+from django.conf.urls.static import static
 
 app_name = CatalogConfig.name
 
 urlpatterns = [
+    path('', home, name='home'),
     path('home/', home, name='home'),
     path('contacts/', contacts, name='contacts'),
-    path('product_info/', product_info, name='product_info'),
+    path('products/<int:product_id>/', product_info, name='product_info'),
     path('add_product/', add_product, name='add_product'),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- catalog/views.py
+++ catalog/views.py
@@ -1,19 +1,10 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, get_object_or_404, redirect
 from .models import Product
 from django import forms
-
-
-# def home(request):
-#     latest_products = Product.objects.order_by('-created_at')[:5]
-#     return render(request, 'home.html', {'latest_products': latest_products})
-
-
-from django.core.paginator import Paginator
 
 def home(request):
     products = Product.objects.all()
     return render(request, 'home.html', {'products': products})
-
 
 def contacts(request):
     success = False
@@ -21,11 +12,9 @@ def contacts(request):
         success = True
     return render(request, 'contacts.html', {'success': success})
 
-
-def product_info(request):
-    products = Product.objects.all()
-    return render(request, 'product_info.html', {'products' : products})
-
+def product_info(request, product_id):
+    product = get_object_or_404(Product, id=product_id)
+    return render(request, 'product_info.html', {'product': product})
 
 class ProductForm(forms.ModelForm):
     class Meta:
@@ -37,7 +26,8 @@ def add_product(request):
         form = ProductForm(request.POST, request.FILES)
         if form.is_valid():
             form.save()
-            return redirect('home')  # Перенаправление на домашнюю страницу
+            return redirect('catalog:home')  # Перенаправление на домашнюю страницу
     else:
         form = ProductForm()
     return render(request, 'add_product.html', {'form': form})
+
