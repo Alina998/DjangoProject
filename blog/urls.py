4a51e51bfa71902a72cfaae8a7cbd87de14a631e
our ame <pastaeva98@mail.ru>
2025-01-26 16:32:00 +0700

    add templates

--- blog/urls.py
+++ blog/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
 from .views import BlogPostListView, BlogPostDetailView, BlogPostCreateView, BlogPostUpdateView
 
-app_name = 'blog'  # Установите имя приложения
+app_name = 'blog'
 
 urlpatterns = [
     path('', BlogPostListView.as_view(), name='blog_list'),
--- blog/views.py
+++ blog/views.py
@@ -1,17 +1,20 @@
-from django.views.generic import ListView, DetailView, CreateView, UpdateView
+from django.shortcuts import render
+from django.views.generic import ListView, DetailView, CreateView, UpdateView, TemplateView
 from .models import BlogPost
+
 
 class BlogPostListView(ListView):
     model = BlogPost
-    template_name = 'blog/blog_list.html'
+    template_name = 'blog_list.html'
     context_object_name = 'posts'
 
     def get_queryset(self):
         return BlogPost.objects.filter(is_published=True)
 
+
 class BlogPostDetailView(DetailView):
     model = BlogPost
-    template_name = 'blog/blog_detail.html'
+    template_name = 'blog_detail.html'
     context_object_name = 'post'
 
     def get_object(self, queryset=None):
@@ -23,13 +26,13 @@ class BlogPostDetailView(DetailView):
 class BlogPostCreateView(CreateView):
     model = BlogPost
     fields = ['title', 'content', 'preview_image', 'is_published']
-    template_name = 'blog/blog_form.html'
-    success_url = '/blog/'  # перенаправление после создания
+    template_name = 'blog_form.html'
+    success_url = '/'  # перенаправление после создания
 
 class BlogPostUpdateView(UpdateView):
     model = BlogPost
     fields = ['title', 'content', 'preview_image', 'is_published']
-    template_name = 'blog/blog_form.html'
+    template_name = 'blog_form.html'
 
     def get_success_url(self):
         return f'/blog/{self.object.id}/'  # перенаправление на просмотр статьи
